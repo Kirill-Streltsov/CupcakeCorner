a8fefd67ea83d388d378a1815990e962771ddc9a
Kirill-Streltsov <krlsrlsv@gmail.com>
2023-08-01 01:02:00 +0200

    IMPORTANT: Encoding an observable object class

--- CupcakeCorner/ContentView.swift
+++ CupcakeCorner/ContentView.swift
@@ -8,17 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @StateObject var order = Order()
+    
     var body: some View {
-        AsyncImage(url: URL(string: "https://avatars.steamstatic.com/649cc96f28905739f4e62c9db0ffec4f6c34c50a.jpg")) { phase in
-            if let image = phase.image {
-                image // Displays the loaded image.
-            } else if phase.error != nil {
-                Color.red // Indicates an error.
-            } else {
-                ProgressView()
+        NavigationView {
+            Form {
+                Section {
+                    Picker("Select your cake type", selection: $order.type) {
+                        ForEach(Order.types.indices) {
+                            Text(Order.types[$0])
+                        }
+                    }
+                    Stepper("Number of cakes: \(order.quantity)", value: $order.quantity, in: 3...20)
+                }
+                Section {
+                    Toggle("Any special requests?", isOn: $order.specialRequestEnabled.animation())
+                    
+                    if order.specialRequestEnabled {
+                        Toggle("Add extra frosting", isOn: $order.extraFrosting)
+                        Toggle("Add extra sprinkles", isOn: $order.addSprinkles)
+                    }
+                }
+                NavigationLink {
+                    AddressView(order: order)
+                } label: {
+                    Text("Delivery Details")
+                }
             }
+            .navigationTitle("Cupcake Corner")
         }
-
     }
 }
 
