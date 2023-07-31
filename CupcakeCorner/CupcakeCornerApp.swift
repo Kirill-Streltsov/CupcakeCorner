d6bffa0910eea9449fb898b69ee48abba9b4293a
Kirill-Streltsov <krlsrlsv@gmail.com>
2023-07-31 23:11:32 +0200

    Initial Commit

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Kirill Streltsov on 31.07.23.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
