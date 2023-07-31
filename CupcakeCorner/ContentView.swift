962e2f4fa51d2af2983cc3a2d6e9da312326e6e5
Kirill-Streltsov <krlsrlsv@gmail.com>
2023-08-01 00:13:05 +0200

    could way to load images with AsyncImage (full control with error handling, ProgressView() etc.)

--- CupcakeCorner/ContentView.swift
+++ CupcakeCorner/ContentView.swift
@@ -9,13 +9,16 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        AsyncImage(url: URL(string: "https://avatars.steamstatic.com/649cc96f28905739f4e62c9db0ffec4f6c34c50a.jpg")) { phase in
+            if let image = phase.image {
+                image // Displays the loaded image.
+            } else if phase.error != nil {
+                Color.red // Indicates an error.
+            } else {
+                ProgressView()
+            }
         }
-        .padding()
+
     }
 }
 
